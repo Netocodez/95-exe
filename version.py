a04c794e00c6943a8f3c2b85edc6edd5532961d6
Netocodez <144063530+Netocodez@users.noreply.github.com>
2026-08-28 06:15:28 +0100

    updated version

--- version.py
+++ version.py
@@ -1,10 +1,9 @@
 """Application version and update configuration."""
-
 __version__ = "1.0.0"
+
 APP_NAME = "2nd & 3rd 95 Analyzer"
 APP_EXE_NAME = "2nd-and-3rd-95-analysis.exe"
 
-# Public GitHub manifest. Change only if the repository/manifest location changes.
 UPDATE_MANIFEST_URL = (
-    "https://raw.githubusercontent.com/netocodez/2nd-and-3rd-95-analysis/main/update.json"
+    "https://raw.githubusercontent.com/netocodez/95-exe/main/update.json"
 )
